bac66e6dc721efcf5b262dc5f00d2bf6362598cd
Todd Martin <kj4ntv@gmail.com>
2022-10-02 20:30:20 -0700

    Implement delay function

---
+++ VodnikOS/kern/delay.c
@@ -0,0 +1,9 @@
+#include "delay.h"
+
+/**
+ * A delay function
+ */
+void delay(unsigned int count) {
+	asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+		 : "=r"(count): [count]"0"(count) : "cc");
+}
---
+++ VodnikOS/kern/delay.h
@@ -0,0 +1,9 @@
+#ifndef DELAY_H
+
+/**
+ * A delay function
+ * Compiler will not optimize this delay away
+ */
+void delay(unsigned int count);
+
+#endif
